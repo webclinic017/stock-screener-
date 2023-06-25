0266c34afb55f3ab04ac555a64bb6674d6f07fad
root <jmalone@jwaresolutions.com>
2023-06-25 07:24:09 +0000

    alpaca api update

--- alpaca_api.py
+++ alpaca_api.py
@@ -3,20 +3,26 @@ import requests
 from alpaca_auth import alpacaAuth
 
 class AlpacaAPI:
-    def __init__(self, endpoint, key, secret):
-        alpaca_api_key = alpacaAuth.alpaca_api_key
-        alpaca_secret_key = alpacaAuth.alpaca_secret_key
-        alpaca_endpoint = alpacaAuth.alpaca_endpoint
-        self.api = tradeapi.REST(alpaca_api_key, alpaca_secret_key, alpaca_endpoint, api_version='v2')
-
+    def __init__(self):
+        self.alpaca_api_key = alpacaAuth.alpaca_api_key
+        self.alpaca_secret_key = alpacaAuth.alpaca_secret_key
+        self.alpaca_endpoint = alpacaAuth.alpaca_endpoint
+        self.authenticate()
+  
     def authenticate(self):
-        # Authenticate the user with the Alpaca API
-        pass
+        try:
+            self.api = tradeapi.REST(self.alpaca_api_key, self.alpaca_secret_key, self.alpaca_endpoint, api_version='v2')
+            return self.api
+        except Exception as e:
+            # Handle authentication error
+            print("Authentication failed:", str(e))
+            return None
+
 
     def get_market_status(self):
         # Retrieve the current market status (open or closed)
         url = "https://api.alpaca.markets/v2/clock"
-        response = requests.get(url)
+        response = requests.get(self.alpaca_api_key, self.alpaca_secret_key, url, api_version='v2')
         if response.status_code == 200:
             market_status = response.json()["is_open"]
             return True
@@ -24,10 +30,12 @@ class AlpacaAPI:
             return False
 
     def get_account_information(self):
-        # Retrieve the user's account information
-        pass
-
-
-
-test = AlpacaAPI()
-print(test.get_market_status())
+        #Retrieve the user's account information
+        try:
+            account = self.api.get_account()
+            return account
+        except Exception as e:
+            # Handle API error
+            print("Failed to retrieve account information:", str(e))
+            return None
+        
